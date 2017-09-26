cc5a5e4b9ea5daf28468afe8a1f04b19680f5c30
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-09-25 16:06:30 -1000

    can access data, now using DOM to create and append data

--- public/js/app.js
+++ public/js/app.js
@@ -5,23 +5,31 @@
 // when "My Boards" is clicked
 // create an xhr get request to http://www.reddit.com/r/MY_FAVORITE_SUBBREDDIT.json
 
-
+//get 'My Boards' menu item
 let myBoardsMenu = document.getElementById('myBoards');
+
+//click event listener added to 'My Boards' menu item
 myBoardsMenu.addEventListener('click', function (e) {
-  var myBoardsXHR = new XMLHttpRequest();
-  myBoardsXHR.addEventListener('load', function () {
-  var myBoardsObj = JSON.parse(this.responseText);
-  for (var i = 0; i < myBoardsObj.length; i++) {
-    console.log(myBoardsObj[i]);
-  }
+  //new xhr load request initiated on 'click'
+  let myBoardsReq = new XMLHttpRequest();
+  myBoardsReq.addEventListener('load', function () {
+    //gets object data
+    let myBoardsObj = JSON.parse(this.responseText);
+    //create new variable to access data.children
+    let myBoardsObjData = myBoardsObj.data.children;
 
 
-  let myBoardsContent = document.getElementById('innerContent').innerHTML = myBoardsObj;
+    for (var i = 0; i < myBoardsObjData.length; i++) {
 
+
+      console.log(myBoardsObjData[i]);
+
+      let myBoardsContent = document.getElementById('innerContent');
+      myBoardsContent.innerHTML = myBoardsObj[i];
+    }
   });
-  myBoardsXHR.open("GET", "http://www.reddit.com/r/Futurology.json");
-  myBoardsXHR.send();
-
+  myBoardsReq.open("GET", "http://www.reddit.com/r/Futurology.json");
+  myBoardsReq.send();
 });
 
 
