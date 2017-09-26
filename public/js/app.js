798c8908c5256b6a102d3775ffa2b67908eb7dea
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-09-25 15:29:44 -1000

    basic layout set, now working on API info

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,34 @@
-console.log('sanity check');
+// When the page loads we need to have the application go out to the API endpoint of your choosing to pull in the current feed via AJAX.
+
+
+// Interaction, Loading Data
+// when "My Boards" is clicked
+// create an xhr get request to http://www.reddit.com/r/MY_FAVORITE_SUBBREDDIT.json
+
+
+let myBoardsMenu = document.getElementById('myBoards');
+myBoardsMenu.addEventListener('click', function (e) {
+  var myBoardsXHR = new XMLHttpRequest();
+  myBoardsXHR.addEventListener('load', function () {
+  var myBoardsObj = JSON.parse(this.responseText);
+  for (var i = 0; i < myBoardsObj.length; i++) {
+    console.log(myBoardsObj[i]);
+  }
+
+
+  let myBoardsContent = document.getElementById('innerContent').innerHTML = myBoardsObj;
+
+  });
+  myBoardsXHR.open("GET", "http://www.reddit.com/r/Futurology.json");
+  myBoardsXHR.send();
+
+});
+
+
+// when "Random" is clicked
+// create an xhr get request to http://www.reddit.com/r/ANOTHER_SUBBREDDIT.json
+
+
+
+// when "Get the app" is clicked
+// create an xhr get request to http://www.reddit.com/r/ANOTHER_SUBBREDDIT.json
